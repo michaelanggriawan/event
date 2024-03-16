888cb545a3592fd668f5587553b2caebc9b9eb28
michaelanggriawan <ext_michael.anggriawan@malaysiaairlines.com>
2024-03-16 12:41:21 +0700

    feat: add constant configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { EventManagementModule } from './event-management/event-management.module';
+import configuration from './config/constant';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      envFilePath: '.env',
+    }),
     DatabaseModule,
     EventManagementModule,
   ],
