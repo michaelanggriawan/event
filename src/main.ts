a4e055591e51fdeadc1948b793e2c6331477a08b
michaelanggriawan <ext_michael.anggriawan@malaysiaairlines.com>
2024-03-16 12:36:48 +0700

    feat: add interceptors and enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { VersioningType } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { HttpSuccessInterceptor } from './interceptors/success.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.useGlobalInterceptors(new HttpSuccessInterceptor());
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
